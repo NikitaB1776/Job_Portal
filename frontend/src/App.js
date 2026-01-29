48459ef202959abc216be7ee1cd84354b8c5f4a5
Nikita <boradenikita06@gmail.com>
2026-01-29 18:44:15 +0530

    Local frontend changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Login from "./pages//Auth/Login";
+import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 
 function App() {
